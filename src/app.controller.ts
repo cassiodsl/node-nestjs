78cfcc84d87d8a138ca3f012c49d15832d91606a
Cassio Lemos <cassiodsl@gmail.com>
2020-01-16 21:53:04 +1100

    WIP adding authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
-@Controller('test')
+@Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) { }
+    constructor(
+        private readonly appService: AppService,
+        private readonly authService: AuthService) { }
+
+    @UseGuards(AuthGuard('local'))
+    @Post('auth/login')
+    async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
 
     @Get(':id')
     getHello(@Param('id') id): string {
-        console.log(id);
         return this.appService.getHello();
     }
 }
